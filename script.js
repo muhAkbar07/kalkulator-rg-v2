91d5083e551fd76e4afca6e6ad6c86c8a4d23c4d
muhAkbar07 <muhakbar072x@gmail.com>
2026-08-05 22:15:48 +0700

    mengubah perhitungan perpanjangan

--- script.js
+++ script.js
@@ -109,7 +109,15 @@ function calculateGadai(event) {
   ) * 1000;
 
 
-  const perpanjangLewat = Math.ceil((pinjaman * 0.16) / 1000) * 1000;
+  // const perpanjangLewat = Math.ceil((pinjaman * 0.16) / 1000) * 1000;
+  // const adminPerpanjang =
+  // pinjaman < 500000
+  //   ? 5000
+  //   : Math.ceil((pinjaman * 0.01) / 1000) * 1000;
+
+  const perpanjangLewat = Math.ceil(
+    (pinjaman * 0.15 + adminPerpanjang) / 1000
+  ) * 1000;
   
   const tebuLewat = pinjaman + pinjaman * 0.05 + tarif * 0.5;
   const nominalPengganti = pinjaman + pinjaman * 0.1;
